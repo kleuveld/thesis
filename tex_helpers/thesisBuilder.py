391a22e37e50ee258db14d68e7ad356059ccf235
koen <koen.leuveld@outook.com>
2022-06-29 11:44:27 +0200

    update thesisbuilder.py the .aux extension was included in the file definition, but was then stripped.

--- tex_helpers/thesisBuilder.py
+++ tex_helpers/thesisBuilder.py
@@ -22,32 +22,33 @@ class thesisBuilder(PdfBuilder):
         #  pdflatex -interaction=nonstopmode -synctex=1 tex_root
         # note that we append the base_name of the file to the command here
         yield(PDFLATEX + [self.base_name], "Running pdflatex...")
-
+        #yield(PDFLATEX + [thesis.tex], "Running pdflatex...")
+        
         # LaTeXTools has run pdflatex and returned control to the builder
         # here we just add text saying the step is done, to give some feedback
         self.display("done.\n")
 
         # now run bibtex
-        chapters =   ["chapters/cameroontrust/cameroontrust_paper.aux",
-                     "chapters/conclusion/conclusion.aux",
-                     "chapters/congogbv/congogbv.aux",
-                     "chapters/introduction/introduction.aux",
-                     "chapters/n2a_impact/n2a_impact.aux",
-                     "chapters/slfootball/slfootball.aux",
-                     "chapters/slfootball/slfootball.aux",
-                     "chapters/acknowledgements/acknowledgements.aux",
-                     "chapters/summary_dutch/summary_dutch.aux",
-                     "chapters/summary_english/summary_english.aux"
+        chapters =   ["chapters/cameroontrust/cameroontrust_paper",
+                     "chapters/conclusion/conclusion",
+                     "chapters/congogbv/congogbv",
+                     "chapters/introduction/introduction",
+                     "chapters/n2a_impact/n2a_impact",
+                     "chapters/slfootball/slfootball",
+                     "chapters/acknowledgements/acknowledgements",
+                     "chapters/summary_dutch/summary_dutch",
+                     "chapters/summary_english/summary_english"
                      ]
         self.display("Running bibtex...\n")
         for file in chapters:
-            yield(BIBTEX + [file.rstrip(".aux")],"  (%s)\n" % file)
+            yield(BIBTEX + [file],"  (%s)\n" % file)
             self.display("done.\n")
 
 
         # second run of pdflatex
         yield(
             PDFLATEX + [self.base_name],
+            #PDFLATEX + [thesis.tex],
             "Running pdflatex again..."
         )
 
@@ -56,6 +57,7 @@ class thesisBuilder(PdfBuilder):
         # third run of pdflatex
         yield(
             PDFLATEX + [self.base_name],
+            #PDFLATEX + [self.base_name]
             "Running pdflatex for the last time..."
         )
 
